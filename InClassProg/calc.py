b9be8a5b4bb36652bd837179bb50a9ff8fd77f9b
saxtonvandalsen <saxtonvandalsen@gmail.com>
2025-02-07 19:34:33 -0800

    intro to scheme syntax/logic

--- InClassProg/calc.py
+++ InClassProg/calc.py
@@ -1,34 +1,40 @@
 memory = 0
 
+# Get and store global result of adding x and y
 def add(x, y):
     global memory
     result = x + y
     memory = result
     return result
 
+# Get and store global result of x subtracted y
 def sub(x, y):
     global memory
     result = x - y
     memory = result
     return result
 
+# Get and store global result of multiplication of two numbers
 def mul(x, y):
     global memory
     result = x * y
     memory = result
     return result
 
+# Get and store global result of dividing of two numbers
 def div(x, y):
     global memory
     result = x / y
     memory = result
     return result
 
+# Get and store global result of remainder of x divided by y
 def mod(x, y):
     global memory
     result = x % y
     memory = result
     return result
+
 
 if __name__ == "__main__":
     userin = input("Provide Operation: ").upper()
@@ -56,4 +62,3 @@ if __name__ == "__main__":
         if result != "ERROR":
             print("{0} {1} {2} = {3}".format(operand1, operator, operand2, result))
         userin = input("Provide Operation: ").upper()
-
