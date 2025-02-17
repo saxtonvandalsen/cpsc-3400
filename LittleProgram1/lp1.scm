4f0a940a08e44edf130beddc080986e87ec4ec2d
saxtonvandalsen <saxtonvandalsen@gmail.com>
2025-02-17 15:47:32 -0800

    finished recursive function

--- LittleProgram1/lp1.scm
+++ LittleProgram1/lp1.scm
@@ -6,21 +6,30 @@
 ; I need a function to display the output
 ; and specifically make sure its on a new line
 (define (print_output)
-    (display "") ; prints the prompt
-    (display data) ; prints the output
-    (display "\n") ; print on a new line
+    (display "Sum of multiples of 3 or 5: ") ; Prints the prompt
+    (display (sum-multiples-3-or-5 range)) ; Prints the return value of the function
+    (display "\n") ; Print on a new line
 )
 
-; After getting the range of numbers needed, I need to 
-; define a function to recursively iterate through the range
-; and sum up the multiples of 3 and 5
-(define (sum-multiples-3-and-5 range)
-    ; base case using zero? to check if length of range is zero
+; After getting the range of numbers needed below, I need to 
+; define a function to recursively go through the range
+; and sum up the multiples of 3 and 5.
+; I realized I needed to split up the two recursive checks for
+; divisibility by 3 or 5 into separate cond branches to handle each case
+(define (sum-multiples-3-or-5 range)
+    ; Base case using zero? to check if length of range is zero
     (cond ((zero? (length range)) 0)
         
+        ; Checking if value is divisible by 3, if so then add to sum
+        ((zero? (modulo (car range) 3)) (+ (car range) (sum-multiples-3-or-5 (cdr range))))
+        
+        ; Checking if value is divisible by 5, if so then add to sum
+        ((zero? (modulo (car range) 5)) (+ (car range) (sum-multiples-3-or-5 (cdr range))))
+        
+        ; Else case ignore numbers that are not divisible by 3 or 5, move forward
+        (else (sum-multiples-3-or-5 (cdr range)))
     )
 )
-
 
 ; Using iota with specifically declared parameters
 ; that we learned in class to define the range of integers
@@ -28,3 +37,6 @@
 ; Defining num to 29999 for use
 (define num 29999)
 (define range (iota num 1))
+
+; Calling the function to print the output to console
+(print_output)
