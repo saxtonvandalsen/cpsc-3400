5dd02ff2f2788204eb50529f62b3a2192518d58e
saxtonvandalsen <saxtonvandalsen@gmail.com>
2025-03-08 11:32:29 -0800

    added error handling on CL map loading

--- game-3/game3.py
+++ game-3/game3.py
@@ -19,26 +19,81 @@
 #   by player's input.
 # - The dungeon 
 
+# You'll need to get the key from Machinery Technician to get into the engine room to fix the pipe
+
 # After reading through and thinking about how to go about creating my own text-based dungeon crawl
 # style game I decided that an object oriented approach would make the most sense.
 
+import sys
+import json
+
 # Player class
 class Player:
-    def __init__(self, inventory, location):
-        self.inventory = inventory
-        self.location = location
-    
+    def __init__(self, direction, start_location):
+        self.inventory = []
+        self.location = start_location
+        self.direction = direction
 
+    # To get and display the current location of the player in case they forgot
     def checkLocation():
+        print("You're current location is: {location}")
 
-# To define object and how to interact with them    
-class Object:
+    def checkDirection():
+        print("You are currently facing: " {direction})
+
+    # To access and check objects within your current inventory
+    def checkInventory():
+
+    # To access and use an item from your inventory
+    def useItem():
+
+
+
+# To define items throughout the game    
+class Item:
+    def __init__(self, name, description, interactions):
+        self.name = name
+        self.description = description
+        self.interactions = interactions # For the verb actions on items
+
 
 # To handle navigating through through rooms in the ship
 class Ship:
+    def __init__(self, file_path=None):
+        self.rooms = {} # To store rooms from json layout
+        
+        # Useful for the player to know how to run the game with a provided
+        # json "dungeon" style map
+        if not file_path:
+            print("Error: No dungeon map provided.\n Run: python3 game3.py <dungeon.json>")
+            sys.exit(1)
+        
+        self.load_map(file_path) # Loading the provided map on command line
+    
+    # To load in a given map (dungeon)
+    def load_map(self, file_path):
+        # To load in json file from command line in read mode
+        try:
+            with open(file_path, "r") as file:
+                self.rooms = json.load(file)
+        # Used to troubleshoot command line loading json file
+        except FileNotFoundError:
+            print(f"Error: File '{file_path}' not found.")
+            sys.exit(1)
+
 
 # Define each room, the objects in that room, and how you can interact within each
 class Room:
+    def __init__(self, name, description, directions, items):
+        self.name = name
+        self.description = description
+        self.directions = directions
+        self.items = items
 
 # Handle the game flow and logic
 class Game:
+    def __init__(self,):
+
+
+if __name__ == "__main__":
+    
